fe25946256bb4f97231e21be42abc1ae11a8b5a4
Wenting Song <song1992katherine@hotmail.com>
2020-04-15 20:52:40 -0400

    Update 189_Rotate Array.java

--- Java/189_Rotate Array.java
+++ Java/189_Rotate Array.java
@@ -1,3 +1,20 @@
+//Algorithm
+
+// This approach is based on the fact that when we rotate the array k times, k%nk elements from the back end of the array come to the front and the rest of the elements from the front shift backwards.
+
+// In this approach, we firstly reverse all the elements of the array. Then, reversing the first k elements followed by reversing the rest n-kn−k elements gives us the required result.
+
+// Let n = 7n=7 and k = 3k=3.
+
+// Original List                   : 1 2 3 4 5 6 7
+// After reversing all numbers     : 7 6 5 4 3 2 1
+// After reversing first k numbers : 5 6 7 4 3 2 1
+// After revering last n-k numbers : 5 6 7 1 2 3 4 --> Result
+
+// Time complexity: O(n). n elements are reversed a total of three times.
+
+// Space complexity: O(1). No extra space is used.
+
 public class Solution {
     public void rotate(int[] nums, int k) {
         k %= nums.length;
@@ -15,3 +32,4 @@ public class Solution {
         }
     }
 }
+
