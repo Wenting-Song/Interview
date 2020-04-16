b0f93f12b064c5655bbbfc8da450c0586aac1c23
Wenting Song <song1992katherine@hotmail.com>
2020-04-16 14:16:32 -0400

    Update Valid_Parenthesis_String.java

--- Java/Valid_Parenthesis_String.java
+++ Java/Valid_Parenthesis_String.java
@@ -1,7 +1,18 @@
-Check from left to right and then check from right to left.
-When check from left to right, take all '*'s as '(', to see whether can match all ')'s.
-And, When check from right to left, take all '*'s as ')', to see whether can match all '('s.
-If both checks are valid, then the string is valid.
+// Check from left to right and then check from right to left.
+// When check from left to right, take all '*'s as '(', to see whether can match all ')'s.
+// And, When check from right to left, take all '*'s as ')', to see whether can match all '('s.
+// If both checks are valid, then the string is valid.
+
+// Part one:
+// The first loop guarantees there would be no ) left for pairing, but there may be ( or * left.
+// if * left, we can use * as '', there is still a fully match.
+// To check if there are ( left, the second loop comes.
+
+// Part two:
+// the second loop guarantees there would be no ( left for pairing.
+
+// The Intersection of part one and part two guarantees there would be no ) and no ( left for pairing.
+// So this solution works well.
 class Solution {
     public boolean checkValidString(String s) {
         int bal = 0;
@@ -27,3 +38,4 @@ class Solution {
         return true;
     }
 }
+// https://leetcode.com/problems/valid-parenthesis-string/discuss/139759/Java-Very-easy-solution.-No-recursion-dp.
