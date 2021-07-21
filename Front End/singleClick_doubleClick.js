2ee5dbbcd1b415a3ad2b6e3e3562f640e65366e6
Wenting Song <song1992katherine@hotmail.com>
2021-07-21 19:44:28 -0400

    Update singleClick_doubleClick.js

--- Front End/singleClick_doubleClick.js
+++ Front End/singleClick_doubleClick.js
@@ -1,1 +1,16 @@
 
+const button = document.getElementById('button')
+let timer
+button.addEventListener('click', event => {
+  if (event.detail === 1) {
+    timer = setTimeout(() => {
+      console.log('click')
+    }, 200)
+  }
+})
+button.addEventListener('dblclick', event => {
+  clearTimeout(timer)
+  console.log('dblclick')
+})
+
+//<button id="button">Click me</button>
